e335399d72edc0318aa109feaa67d04c929e5357
Phoenix <lankun@student.ubc.ca>
2022-03-31 19:36:07 -0700

    update working

--- final.php
+++ final.php
@@ -329,6 +329,25 @@
                 OCILogoff($db_conn);
             }
 
+            function handleCreateRequest()
+            {
+                global $db_conn;
+                $password = $_POST['password'];
+                $old_email = $_POST['old_email'];
+                $new_email = $_POST['new_email'];
+
+                $query = "SELECT * FROM account WHERE email='" . $old_email . "' AND password='" . $password . "'";
+                $result = executePlainSQL($query);
+                
+                if (oci_fetch_row($result) != false) {
+                    // you need the wrap the old name and new name values with single quotations
+                    executePlainSQL("UPDATE account SET email='" . $new_email . "' WHERE email='" . $old_email . "'");
+                    OCICommit($db_conn);
+                } else {
+                    echo "<br> There was an error updating your email, please double check your email and password <br>";
+                }
+            }
+
             function handleUpdateRequest()
             {
                 global $db_conn;
@@ -396,7 +415,7 @@
                 if (connectToDB()) {
                     if (array_key_exists('resetTablesRequest', $_POST)) {
                         handleResetRequest();
-                    } else if (array_key_exists('updateQueryRequest', $_POST)) {
+                    } else if (array_key_exists('updateEmailRequest', $_POST)) {
                         handleUpdateRequest();
                     } else if (array_key_exists('insertQueryRequest', $_POST)) {
                         handleInsertRequest();
